b189ae6e8329024ddb8f2abb15db1d24b414b22b
Kevin Bonham <kevbonham@gmail.com>
2017-11-06 08:38:51 -0500

    add basic script

---
+++ my_module.py
@@ -0,0 +1,4 @@
+import datetime
+
+my_name = "Kevin"
+today_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
---
+++ my_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from my_module import *
+
+def main():
+    print("Hi {}! It is now {}".format(my_name, today_date))
+
+if __name__ == '__main__':
+    main()
